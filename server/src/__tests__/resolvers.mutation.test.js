37b160ca472bfe5c8cf7ee666be69f893625d2d8
Ruben Padilla <rubbenpad2@gmail.com>
2020-05-01 07:17:58 -0500

    [ADD] login resolver test and implementation

--- server/src/__tests__/resolvers.mutation.test.js
+++ server/src/__tests__/resolvers.mutation.test.js
@@ -4,8 +4,15 @@ const resolvers = require('../graphql/resolvers')
 const jwt = require('jsonwebtoken')
 const { config } = require('../config')
 
+const hashedPassword =
+  '$2b$10$v6KrB3BkAIgsei7.o2t9.eItzfK2Zf7sloAGW12d5O.co4.OVdPX2'
 const id = 'ABCD1234'
-const fakeCompany = { name: 'MGC', email: 'mgc@mail.com', password: 'secret' }
+const fakeCompany = {
+  id,
+  name: 'MGC',
+  email: 'mgc@mail.com',
+  password: 'secret'
+}
 const fakeToken = jwt.sign(
   {
     id,
@@ -24,15 +31,20 @@ const mockContext = {
   }
 }
 
+// Just for easy access to this functions
+const { createCompany, getCompany } = mockContext.dataSources.companyAPI
+
 describe('[Mutation.signup]', () => {
-  const { createCompany, getCompany } = mockContext.dataSources.companyAPI
+  beforeEach(() => {
+    getCompany.mockClear()
+  })
 
   test('Signup should works to register a new company an returns a token', async () => {
     createCompany.mockResolvedValueOnce(id)
 
     const response = await resolvers.Mutation.signup(
       null,
-      { data: fakeCompany },
+      { input: fakeCompany },
       mockContext
     )
     expect(getCompany).toBeCalledWith({ email: fakeCompany.email })
@@ -44,13 +56,69 @@ describe('[Mutation.signup]', () => {
     getCompany.mockResolvedValueOnce(fakeCompany)
 
     try {
+      // Simulate call to API. This should returns an error
+      // that's because expect is used in catch.
+      // I try  to made it with expect(() => {}).toThrow() as Jest
+      // show it in the docs but that doesn't work for this case
       const response = await resolvers.Mutation.signup(
         null,
-        { data: fakeCompany },
+        { input: fakeCompany },
         mockContext
       )
     } catch (error) {
       expect(error).toEqual(new Error('Company email is already in use'))
     }
+  })
+})
+
+describe('Mutation.login', () => {
+  beforeEach(() => {
+    getCompany.mockClear()
+  })
+
+  test('Should allow logging with a registered user', async () => {
+    const storedCompany = { ...fakeCompany, password: hashedPassword }
+    getCompany.mockResolvedValueOnce(storedCompany)
+
+    const response = await resolvers.Mutation.login(
+      null,
+      { input: fakeCompany },
+      mockContext
+    )
+
+    expect(getCompany).toHaveBeenCalledTimes(1)
+    expect(getCompany).toHaveBeenCalledWith({ email: fakeCompany.email })
+
+    expect(response).toEqual(fakeToken)
+  })
+
+  test('Should returns an error when try to login with a no registered user', async () => {
+    const response = await resolvers.Mutation.login(
+      null,
+      {
+        input: { ...fakeCompany, email: 'unregistered@mail.com' }
+      },
+      mockContext
+    )
+
+    expect(getCompany).toHaveBeenCalledTimes(1)
+    expect(getCompany).toHaveBeenCalledWith({ email: 'unregistered@mail.com' })
+    expect(getCompany).toHaveReturnedWith(undefined)
+    expect(response).toEqual(new Error('Company no registered'))
+  })
+
+  test('Should returns an error when password is wrong', async () => {
+    getCompany.mockResolvedValueOnce(fakeCompany)
+    const response = await resolvers.Mutation.login(
+      null,
+      { input: { ...fakeCompany, password: 'wrong' } },
+      mockContext
+    )
+
+    expect(getCompany).toHaveBeenCalledTimes(1)
+    expect(getCompany).toHaveBeenCalledWith({ email: fakeCompany.email })
+    expect(response).toEqual(
+      new Error('Company user or password are incorrect')
+    )
   })
 })
--- server/src/graphql/resolvers.js
+++ server/src/graphql/resolvers.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const { config } = require('../config')
 
@@ -12,9 +13,35 @@ module.exports = {
   },
 
   Mutation: {
-    login: () => {},
-    signup: async (_, { data }, { dataSources }) => {
-      const { email } = data
+    login: async (_, { input }, { dataSources }) => {
+      const { email, password } = input
+
+      const company = await dataSources.companyAPI.getCompany({ email })
+
+      if (!company) {
+        return new Error('Company no registered')
+      }
+
+      const passwordIsValid = await bcrypt.compare(password, company.password)
+
+      if (!passwordIsValid) {
+        return new Error('Company user or password are incorrect')
+      }
+
+      const token = jwt.sign(
+        {
+          id: company.id,
+          name: company.name,
+          email: company.email
+        },
+        config.secret
+      )
+
+      return token
+    },
+
+    signup: async (_, { input }, { dataSources }) => {
+      const { email } = input
 
       // Check if the company already have been created, if true
       // throw a new error
@@ -22,10 +49,10 @@ module.exports = {
       if (company) throw new Error('Company email is already in use')
 
       const createdCompanyId = await dataSources.companyAPI.createCompany({
-        company: data
+        company: input
       })
 
-      const newCompany = { id: createdCompanyId, name: data.name, email }
+      const newCompany = { id: createdCompanyId, name: input.name, email }
       const token = jwt.sign(newCompany, config.secret)
 
       return token
